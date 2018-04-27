43935f55379ff3963bea77bac3c40869b8ba4849
qiuyue <qiuyue9971@126.com>
2018-04-27 20:46:08 +0800

    美化界面

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template,redirect,url_for,request,session
+from flask import render_template,redirect,url_for,request,session,request
 import pymysql
 import os
 from .. import photos
